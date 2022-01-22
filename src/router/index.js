78ec9e4f85b3e29c7f98aae62cffe249ce4dcc38
Ayakie <ayalake156@gmail.com>
2022-01-23 03:32:43 +0900

    [add] 404 not found page / router-guard

--- src/router/index.js
+++ src/router/index.js
@@ -4,37 +4,67 @@ import Welcome from '../views/Welcome.vue'
 import Create from '../views/Create.vue'
 import Detail from '../views/Detail.vue'
 import ListView from '../views/ListView.vue'
+import NotFound from '../views/NotFound.vue'
+import { projectAuth } from "../firebase/config"
+
+const requireAuth = (to, from, next) => {
+    let user = projectAuth.currentUser
+    if (!user) {
+        next({ name: 'Welcome'})
+    } else {
+        next()
+    }
+}
+
+const requireNoAuth = (to, from, next) => {
+    let user = projectAuth.currentUser
+    if (user) {
+        next ({ name: 'Home' })
+    } else {
+        next()
+    }
+}
 
 const routes = [
     {
         path:'/',
         name: 'Welcome',
         component: Welcome,
+        beforeEnter: requireNoAuth
     },
     {
         path: '/home',
         name: 'Home',
         component: Home,
-        meta: { transition: 'slide-left' }
+        meta: { transition: 'slide-left' },
+        beforeEnter: requireAuth
     },
     {
         path: '/create',
         name: 'Create',
         component: Create,
-        props: true
+        props: true,
+        beforeEnter: requireAuth
     },
     {
         path: '/detail/:id',
         name: 'Detail',
         component: Detail,
-        props: true
+        props: true,
+        beforeEnter: requireAuth
     },
     {
         path: '/view/:category',
         name: 'ListView',
         component: ListView,
         props: true,
-        meta: { transition: 'slide-right'}
+        meta: { transition: 'slide-right'},
+        beforeEnter: requireAuth
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound,
     }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="not-found">
+    <img src="@/assets/image/not-found.svg" alt="">
+    <BackPage :goBack="'Home'"/>
+</div>
+</template>
+
+<script>
+import BackPage from "../components/BackPage.vue";
+export default {
+    components: { BackPage }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/css/main';
+
+.not-found {
+    margin: 70px auto;
+}
+
+img {
+    max-width: 300px;
+    margin: 0 auto;
+}
+
+@media (min-width: 744px) {
+    img {
+        max-width: 400px;
+    }
+}
+.back {
+    color: main.$bg-white;
+}
+</style>
